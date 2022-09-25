bd8a30496b2ae82d5a9dc97a5e36f59fabc09a3c
gutsyguy <yalamber.subba@csedge.org>
2022-09-25 04:11:53 -0700

    fifth

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -4,73 +4,130 @@ using UnityEngine;
 
 public class Ship : MonoBehaviour
 {
-    private float speed = 3.0f;
-    private float flightForce = 3.0f;
+    private float speed = 6.0f;
+    private float flightForce = 8.0f;
     private Rigidbody2D rb;
-    private bool isGrounded;
 
     private float flightSpeed = 3.0f;
     private bool isFlying = false;
     private bool isWalking = true;
+    private bool isUpside = false;
     private int FlyDirection = -1;
+
+    private int jump_count = 0;
+    private int max_jump_count = 2;
     // Start is called before the first frame update
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-        
+
 
     }
 
     // Update is called once per frame
 
-    private void FixedUpdate()
+    private void Update()
     {
-        if (isWalking == true){
+        if (isWalking == true)
+        {
+            rb.gravityScale = 3;
             Walk();
         }
-        if (isFlying == true){
+        if (isFlying == true)
+        {
+            rb.gravityScale = 0;
             Fly();
+        }
+        if (isUpside == true)
+        {
+            rb.gravityScale = -3;
+            Upside();
         }
         rb.velocity = new Vector2(speed, rb.velocity.y);
 
+        StartCoroutine(Example());
     }
+
+    IEnumerator Example()
+    {
+        float origin_pos = rb.position.x;
+        yield return new WaitForSeconds(0.25f);
+        float new_pos = rb.position.x;
+        if (origin_pos - new_pos == 0)
+        {
+            //PLACEHOLDER
+            Destroy(gameObject);
+        }
+    }
+
 
     private void OnCollisionEnter2D(Collision2D col)
     {
-        if(col.gameObject.tag == "Ground")
+
+        if (col.gameObject.tag == "Ground")
         {
-            Debug.Log("JUMPABLE");
-            isGrounded = true;
+            jump_count = 0;
         }
         else if (col.gameObject.tag == "obstacle")
-        {}
+        {
+            //PLACEHOLDER
+            Destroy(gameObject);
+        }
+        else if (col.gameObject.tag == "enemy")
+        {
+            if (rb.velocity.x <= 0)
+            {
+                Destroy(col.gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
     }
-    
+
     void OnTriggerEnter2D(Collider2D col)
     {
-        Debug.Log("WORK");
         if (col.gameObject.tag == "FlyPortal")
         {
             isFlying = true;
             isWalking = false;
+            isUpside = false;
         }
         else if (col.gameObject.tag == "WalkPortal")
         {
-            isFlying = true;
-            isWalking = false;
+            isFlying = false;
+            isWalking = true;
+            isUpside = false;
         }
-        Debug.Log(isFlying);
+        else if (col.gameObject.tag == "UpsidePortal")
+        {
+            isFlying = false;
+            isWalking = false;
+            isUpside = true;
+        }
     }
-        
-        
-    
+
+    private void Upside()
+    {
+        Debug.Log(rb.velocity);
+        FlyDirection = 0;
+        if (Input.GetKeyDown(KeyCode.Space) && max_jump_count > jump_count)
+        {
+            rb.velocity = new Vector2(speed, flightForce);
+            rb.AddForce(Vector2.down * flightForce * 2, ForceMode2D.Impulse);
+            jump_count++;
+        }
+    }
+
     private void Walk()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
+        FlyDirection = 0;
+        if (Input.GetKeyDown(KeyCode.Space) && max_jump_count > jump_count)
         {
             rb.velocity = new Vector2(speed, flightForce);
             rb.AddForce(Vector2.up * flightForce, ForceMode2D.Impulse);
-            isGrounded = false;
+            jump_count++;
         }
     }
     private void Fly()
@@ -81,7 +138,8 @@ public class Ship : MonoBehaviour
             {
                 FlyDirection = 1;
             }
-            else {
+            else
+            {
                 FlyDirection = -1;
             }
         }
